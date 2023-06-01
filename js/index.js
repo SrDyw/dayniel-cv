01f5fee49d37067af3506eba717a66f00c5e9647
SrDyw <mdayniel@gmail.com>
2023-06-01 10:21:44 -0400

    resposive

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+const id_mail = document.getElementById('mail');
 const download_link = document.querySelector('.download_link');
 
 download_link.addEventListener('click', (e) => {
@@ -5,3 +6,17 @@ download_link.addEventListener('click', (e) => {
     html2pdf(document.getElementById('id_container'));
 })
 
+
+
+id_mail.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    copyToClipboard();
+})
+
+async function copyToClipboard() {
+    const mail_url = document.getElementById('mail_url').innerText;
+
+    const result = await navigator.clipboard.writeText(mail_url);
+    console.log('copied');
+}
