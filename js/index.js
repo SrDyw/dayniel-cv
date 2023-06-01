a5e15190f8cc7fa9e5f997838b98468f6675a54d
SrDyw <mdayniel@gmail.com>
2023-06-01 10:49:43 -0400

    clipboard function

--- js/index.js
+++ js/index.js
@@ -1,22 +1,29 @@
-const id_mail = document.getElementById('mail');
-const download_link = document.querySelector('.download_link');
+const id_mail = document.getElementById("mail");
+const download_link = document.querySelector(".download_link");
 
-download_link.addEventListener('click', (e) => {
+download_link.addEventListener("click", (e) => {
     e.preventDefault();
-    html2pdf(document.getElementById('id_container'));
-})
+    html2pdf(document.getElementById("id_container"));
+});
 
+const preventDefault = document.querySelectorAll("preventDefault");
 
+preventDefault.forEach((element) => {
+    element.addEventListener("click", (e) => e.preventDefault());
+});
 
-id_mail.addEventListener('click', (e) => {
-    e.preventDefault();
+async function copyToClipboard(data) {
+    // const mail_url = document.getElementById('mail_url').innerText;
+
+    const result = await navigator.clipboard.writeText(data);
+
+    const clipanim = document.querySelector(".clipboard_alert");
     
-    copyToClipboard();
-})
-
-async function copyToClipboard() {
-    const mail_url = document.getElementById('mail_url').innerText;
-
-    const result = await navigator.clipboard.writeText(mail_url);
-    console.log('copied');
+    if (!clipanim.classList.contains("animation")) {
+        clipanim.classList.add("animation");
+        setTimeout(() => {
+            console.log("end Animatino");
+            clipanim.classList.remove("animation");
+        }, 2000);
+    }
 }
